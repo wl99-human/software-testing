db4885bad3704ef7ad45c7a54ff4644d3dd065d3
wl99-human <weileelim2012@gmail.com>
2023-05-28 21:45:36 +0800

    Parameterized Testing

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -3,6 +3,8 @@ package com.amigoscode.testing.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,41 +15,17 @@ public class PhoneNumberValidatorTest {
         underTest = new PhoneNumberValidator();
     }
 
-    @Test
-    void itShouldValidatePhoneNumber() {
-        // Given
-        String phoneNumber = "+6512345678";
-
+    @ParameterizedTest
+    @CsvSource({
+            "+6512345678, True",
+            "+651234567890, False",
+            "65123456789, False"
+    })
+    void itShouldValidatePhoneNumber(String phoneNumber, String expected) {
         // When
         boolean isValid = underTest.test(phoneNumber);
 
         // Then
-        assertThat(isValid).isTrue();
-    }
-
-    @Test
-    @DisplayName("Should fail when length is bigger than 11")
-    void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan11() {
-        // Given
-        String phoneNumber = "+651234567890";
-
-        // When
-        boolean isValid = underTest.test(phoneNumber);
-
-        // Then
-        assertThat(isValid).isFalse();
-    }
-
-    @Test
-    @DisplayName("Should fail when does not start with +")
-    void itShouldValidatePhoneNumberWhenDoesNotStartsWithPlusSign() {
-        // Given
-        String phoneNumber = "65123456789";
-
-        // When
-        boolean isValid = underTest.test(phoneNumber);
-
-        // Then
-        assertThat(isValid).isFalse();
+        assertThat(isValid).isEqualTo(Boolean.valueOf(expected));
     }
 }
